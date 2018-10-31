65e51fe6d761fc1f33626f98638f1a766c5687bd
David Nuon <davidnuongm@gmail.com>
2018-10-31 02:03:34 -0700

    MOTD is only retrieved once

--- very-serious-gopher-server/src/Main.hs
+++ very-serious-gopher-server/src/Main.hs
@@ -24,31 +24,32 @@ linesToMOTD host port messageLines = let
    in
       map toListing messageLines
 
-appBuilder :: T.Text -> PortNumber -> GopherApp
-appBuilder host port = let
-      topRow name =  Record { recName = name
+topRow :: T.Text -> PortNumber -> T.Text -> Record
+topRow host port name =  Record { recName = name
       , recSelector = name
       , recHost = host
       , recPort = port
       , recOther = []
       }
 
+
+appBuilder :: T.Text -> PortNumber -> [Listing] -> GopherApp
+appBuilder host port motd = let
       postNames = (filter (\x -> (T.isSuffixOf ".post") (T.pack x)) ) <$> (getDirectoryContents "./data")
       postContent = (mapM readFile) =<< (map (\x -> "./data/" ++ x)) <$> postNames
       lookupTable = zip <$> postNames <*> postContent
-
-      motd = linesToMOTD host port <$> splitOn "\n" <$> readFile "./motd.txt"
-      postIndex = map (\(name, _) -> Listing PlainText $ (topRow . T.pack) name) <$> lookupTable
+      postIndex = map (\(name, _) -> Listing PlainText $ ((topRow host port) . T.pack) name) <$> lookupTable
    in
-      \_ -> Items <$> liftA2 (++) motd postIndex
+      \_ -> Items <$> liftA2 (++) (pure motd) postIndex
 
 main :: IO ()
 main = do
    dataPath <- getEnv "GOPHER_POST_DATA"
    serverHost <- T.pack <$> getEnv "GOPHER_HOST"
    serverPort <- (\port -> read port :: PortNumber) <$> getEnv "GOPHER_PORT"
+   motd <- linesToMOTD serverHost serverPort <$> splitOn "\n" <$> readFile "./motd.txt"
    putStrLn $ dataPath
    putStrLn $ show serverHost
    putStrLn $ show serverPort
-   runGopherApp serverPort $ appBuilder serverHost serverPort
+   runGopherApp serverPort $ appBuilder serverHost serverPort motd
    (forever (threadDelay 1))
