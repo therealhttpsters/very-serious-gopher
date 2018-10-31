112a61e689d593b49c8e8f4f8b10b173cf5c7ab8
David Nuon <davidnuon@pop-os.localdomain>
2018-10-31 01:54:35 -0700

    Got posts listing

--- very-serious-gopher-server/src/Main.hs
+++ very-serious-gopher-server/src/Main.hs
@@ -3,10 +3,14 @@
 import Network.Gopher
 import Control.Concurrent
 import Control.Monad
-import System.Environment
-import Network.Socket
-import Data.List.Split
+import Control.Applicative
 
+import System.Environment
+import System.Directory
+
+import Network.Socket
+
+import Data.List.Split
 import qualified Data.Text          as T
 
 linesToMOTD :: T.Text -> PortNumber  -> [[Char]] -> [Listing]
@@ -22,24 +26,28 @@ linesToMOTD host port messageLines = let
 
 appBuilder :: T.Text -> PortNumber -> GopherApp
 appBuilder host port = let
-      topRow =  Record { recName = "Very Serious Things are Coming"
-      , recSelector = ""
+      topRow name =  Record { recName = name
+      , recSelector = name
       , recHost = host
       , recPort = port
       , recOther = []
       }
 
-      title = [Listing Info topRow]
-      motd = splitOn "\n" <$> readFile "./motd.txt"
+      postNames = (filter (\x -> (T.isSuffixOf ".post") (T.pack x)) ) <$> (getDirectoryContents "./data")
+      postContent = (mapM readFile) =<< (map (\x -> "./data/" ++ x)) <$> postNames
+      lookupTable = zip <$> postNames <*> postContent
+
+      motd = linesToMOTD host port <$> splitOn "\n" <$> readFile "./motd.txt"
+      postIndex = map (\(name, _) -> Listing PlainText $ (topRow . T.pack) name) <$> lookupTable
    in
-      \_ -> Items <$> (\l -> title ++ (linesToMOTD host port l)) <$> motd
+      \_ -> Items <$> liftA2 (++) motd postIndex
 
 main :: IO ()
 main = do
    dataPath <- getEnv "GOPHER_POST_DATA"
    serverHost <- T.pack <$> getEnv "GOPHER_HOST"
    serverPort <- (\port -> read port :: PortNumber) <$> getEnv "GOPHER_PORT"
-   putStrLn dataPath
+   putStrLn $ dataPath
    putStrLn $ show serverHost
    putStrLn $ show serverPort
    runGopherApp serverPort $ appBuilder serverHost serverPort
